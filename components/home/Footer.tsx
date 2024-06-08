553225a1f6c1c74a3487d3ac32ec4e2770b48e5f
Dhruv Sharma <dhruvamsh@gmail.com>
2024-06-08 10:52:15 +0530

    Upadtes routes

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -17,8 +17,8 @@ function Footer({}: Props) {
             </a>
                     <p className="leading-relaxed my-6">Copyright © 2024 Echoverse</p>
                     <div className="flex items-center gap-4">
-                        <a className="generic-hover" href="">
-                        <span className="sr-only">GitHub</span>
+                        <a className="generic-hover" href="https://github.com/DhruvSharma19/EchoVerse">
+                        <span className="sr-only cursor-pointer text-white">GitHub</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" className="fill-current h-5 text-zinc-700 dark:text-zinc-400"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
                         </svg>
                         </a>
@@ -30,13 +30,13 @@ function Footer({}: Props) {
                         <h3 className="text-white mb-4">Docs</h3>
                         <ul className='space-y-2'>
                             <li>
-                                <a className="flex items-start button-hover" target="_self" href="/"><span>Getting Started</span></a>
+                                <a className="flex items-start button-hover " target="_self" href="/"><span className='cursor-pointer'>Getting Started</span></a>
                             </li>
                             <li>
-                                <a className="flex items-start button-hover" target="_self" href="/"><span>Guides</span></a>
+                                <a className="flex items-start button-hover" target="_self" href="/" ><span className='cursor-pointer' >Guides</span></a>
                             </li>
                             <li>
-                                <a className="flex items-start button-hover" target="_self" href="/"><span>API</span></a>
+                                <a className="flex items-start button-hover" target="_self" href="/"><span className='cursor-pointer' >API</span></a>
                             </li>
                         </ul>
                     </div>
@@ -45,7 +45,7 @@ function Footer({}: Props) {
                         <ul className='space-y-2'>
                             <li>
                                 <a className="flex items-start button-hover" target="_blank" href="">
-                                    <span>Effectful</span>
+                                    <span className='cursor-pointer'>Effectful</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 14" className="fill-current h-3 mt-0.5 ml-0.5"><path d="M9.31328 2.625H9.75078V3.0625V9.1875V9.625H8.87578V9.1875V4.11797L1.96602 11.0277L1.65703 11.3367L1.03906 10.7187L1.34805 10.4098L8.25781 3.5H3.18828H2.75078V2.625H3.18828H9.31328Z"></path>
                                     </svg>
                                 </a>
@@ -57,19 +57,19 @@ function Footer({}: Props) {
                         <ul className='space-y-2'>
                             <li>
                                 <a className="flex items-start button-hover" target="_blank" href="">
-                                    <span>Terms & Conditions</span>
+                                    <span className='cursor-pointer'>Terms & Conditions</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 14" className="fill-current h-3 mt-0.5 ml-0.5"><path d="M9.31328 2.625H9.75078V3.0625V9.1875V9.625H8.87578V9.1875V4.11797L1.96602 11.0277L1.65703 11.3367L1.03906 10.7187L1.34805 10.4098L8.25781 3.5H3.18828H2.75078V2.625H3.18828H9.31328Z"></path>
                                     </svg>
                                 </a>
                             </li><li>
                                 <a className="flex items-start button-hover" target="_blank" href="">
-                                    <span>Privacy & Policy</span>
+                                    <span className='cursor-pointer'>Privacy & Policy</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 14" className="fill-current h-3 mt-0.5 ml-0.5"><path d="M9.31328 2.625H9.75078V3.0625V9.1875V9.625H8.87578V9.1875V4.11797L1.96602 11.0277L1.65703 11.3367L1.03906 10.7187L1.34805 10.4098L8.25781 3.5H3.18828H2.75078V2.625H3.18828H9.31328Z"></path>
                                     </svg>
                                 </a>
                             </li>
                             <li>
-                                <a className="flex items-start button-hover" target="_self" href="/"><span>Contact Us</span></a>
+                                <a className="flex items-start button-hover" target="_self" href="/"><span className='cursor-pointer'>Contact Us</span></a>
                             </li>
                         </ul>
                     </div>
